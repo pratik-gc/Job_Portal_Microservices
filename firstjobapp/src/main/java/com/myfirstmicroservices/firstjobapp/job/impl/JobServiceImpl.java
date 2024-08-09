80f468a722188c7a6ca365da8582c6242647efe3
Pratik Chaudhari <pratikcg12@outlook.com>
2024-08-09 12:00:10 +0530

    basic job model ready with some endpoints ready

---
+++ firstjobapp/src/main/java/com/myfirstmicroservices/firstjobapp/job/impl/JobServiceImpl.java
@@ -0,0 +1,26 @@
+package com.myfirstmicroservices.firstjobapp.job.impl;
+
+import com.myfirstmicroservices.firstjobapp.job.Job;
+import com.myfirstmicroservices.firstjobapp.job.JobService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+    private List<Job> jobs = new ArrayList<>();
+    private Long nextId = 1L;
+
+    @Override
+    public List<Job> findAll() {
+        return jobs;
+    }
+
+    @Override
+    public void createJob(Job job) {
+        job.setId(nextId++);
+        jobs.add(job);
+    }
+}
